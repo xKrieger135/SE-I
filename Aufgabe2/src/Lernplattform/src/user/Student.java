bde87f243de595b5a06fe13629041cdb25e6b3d5
Blackgen <jandb07@gmx.de>
2014-11-10 18:05:46 +0100

    edited Student.java

--- Aufgabe2/src/Lernplattform/src/user/Student.java
+++ Aufgabe2/src/Lernplattform/src/user/Student.java
@@ -4,4 +4,22 @@ package user;
  * Created by patrick_steinhauer on 10.11.2014.
  */
 public class Student {
+    private String name;
+    private String vorname;
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
